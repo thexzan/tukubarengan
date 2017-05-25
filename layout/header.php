68f5c45890469a915b2e89db10f74611726467b0
Ikhsan Hadi Nugroho <hello@thexzan.com>
2017-05-25 21:26:07 +0700

    change title

--- layout/header.php
+++ layout/header.php
@@ -60,7 +60,7 @@ function get_qty_order($id_produk){
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>GroupBuyID - Group Buying and Kickstarter Media</title>
+    <title>TukuBarengan - Pokoke Barengan Ben Murah!</title>
 
     <link rel="stylesheet" href="<?php echo base_url; ?>/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo base_url; ?>/assets/css/bootstrap.min.css">
